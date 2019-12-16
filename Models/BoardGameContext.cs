cb5602b6bea9e52f8c8bd3cba687faeedda13b68
labarruffe <lucas_barruffe@hotmail.com>
2019-12-15 22:45:10 -0300

    feat: Adjust BoardGameContext based on models defined

--- Models/BoardGameContext.cs
+++ Models/BoardGameContext.cs
@@ -4,9 +4,30 @@ namespace board_game_api.Models
 {
     public class BoardGameContext: DbContext
     {
-
         public BoardGameContext(DbContextOptions<BoardGameContext> options): base(options) {}
         public DbSet<BoardGame> BoardGame { get; set; }
-        public DbSet<Player> Player { get; set; }
+        public DbSet<Square> Squares { get; set; }
+        public DbSet<Rule> Rules { get; set; }
+        public DbSet<Player> Players { get; set; }
+       
+
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //     builder.Entity<Square>()
+        //         .HasOne<Rule>(r => r.Rule)
+        //         .WithOne(s => s.Square)
+        //         .HasForeignKey<Rule>(b => b.SquareId);
+        // }
+
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //     base.OnModelCreating(builder);
+
+        //     builder.Entity<Square>().ToTable("Squares");
+        //     builder.Entity<Square>().HasKey(p => p.Id);
+        //     builder.Entity<Square>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+
+
+        // }
     }
 }
